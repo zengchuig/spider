b1a9f46d57c511869e68017475951e5230e7e508
zengchuig <2903950426@qq.com>
2020-07-09 22:30:14 +0800

    Delete some files.

--- spider/weixin1/k1.sh
+++
@@ -1,1 +0,0 @@
-sudo /opt/kafka_2.13-2.5.0/bin/zookeeper-server-start.sh /opt/kafka_2.13-2.5.0/config/zookeeper.properties
--- spider/weixin1/k2.sh
+++
@@ -1,1 +0,0 @@
-sudo /opt/kafka_2.13-2.5.0/bin/kafka-server-start.sh /opt/kafka_2.13-2.5.0/config/server.properties
